313a3595a439482f50d6cfb700f62a08a60cf50e
Alex <bojkosasa78@gmail.com>
2025-09-05 20:23:32 +0200

    Blank Secured Page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <h1 className={"text-black"}>Dashboard</h1>
+        </div>
+    );
+}
