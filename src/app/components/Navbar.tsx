534863d50fd211bb1862febffb8b22f5c1ad6ceb
Alex <bojkosasa78@gmail.com>
2025-09-19 19:48:57 +0200

    Navbar created with user info check

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import "./Navbar.css";
+import Link from "next/link";
+import { User } from "lucide-react";
+import {UserInfo, useUser} from "@/hooks/useUser";
+
+
+export default function Navbar({ initialUser }: { initialUser: UserInfo }) {
+    const { user, loading } = useUser();
+
+    return (
+        <div className={"navbar"}>
+            <ul className={"navbar__links"}>
+                <li>
+                    <Link href={"/"}>Home</Link>
+                </li>
+                <li>
+                    <Link href={"/dashboard"}>Dashboard</Link>
+                </li>
+                {loading && !initialUser ? (
+                    <></>
+                ) : (initialUser || user) ? (
+                    <li>
+                        <Link href={"/logout"}>Logout</Link>
+                    </li>
+                ): <>
+                    <li>
+                        <Link href={"/login"}>Login</Link>
+                    </li>
+                    <li>
+                        <Link href={"/signup"}>Signup</Link>
+                    </li>
+                </>}
+
+            </ul>
+            {loading && !initialUser ? (
+                <div className={"navbar__user"}>Loading...</div>
+            ) : (initialUser || user) ? (
+                <div className={"navbar__user"}>
+                    {(initialUser || user)?.username} <User />
+                </div>
+            ): null}
+
+        </div>
+    );
+}
