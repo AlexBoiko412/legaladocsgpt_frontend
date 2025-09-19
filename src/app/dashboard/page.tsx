0f8ed949ceef3ce751924be3fb1d49228227d79f
Alex <bojkosasa78@gmail.com>
2025-09-19 19:47:29 +0200

    Safety check of user info from headers

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,26 @@
-export default function Dashboard() {
+import { headers } from "next/headers";
+
+export default async function DashboardPage() {
+    const h = await headers();
+    const userHeader = h.get("x-user-info");
+
+
+    let user = null;
+    if (userHeader) {
+        try {
+            user = JSON.parse(userHeader);
+        } catch (err) {
+            console.error("Failed to parse x-user-info:", err);
+        }
+    }
+
+    if (!user) return <div>Unauthorized</div>;
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <h1 className={"text-black"}>Dashboard</h1>
+        <div>
+            <h1>Welcome, {user.username}</h1>
+            <p>Email: {user.email}</p>
+            <p>Role: {user.role}</p>
         </div>
     );
 }
