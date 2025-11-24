5a2d874ba28d4dcc975ee69ce9955993f2485b0a
Alex <bojkosasa78@gmail.com>
2025-11-24 16:19:49 +0100

    misc

--- src/app/components/UI/GoogleAuthButton.tsx
+++ src/app/components/UI/GoogleAuthButton.tsx
@@ -1,13 +1,14 @@
 import "./GoogleAuthButton.css"
 import Image from "next/image";
 import googleImage from "../../../static/5847f9cbcef1014c0b5e48c8.png"
+
 const GoogleAuthButton = () => {
     return (
         <button
             type={"button"}
             className={"google_auth_button"}
             onClick={() => window.location.href = "http://localhost:8080/api/auth/oauth2/authorization/google"}>
-            Signup with Google
+            Log In with Google
             <Image
                 height={24}
                 width={24}
